27d50f539eda96a15efb3bb4d52a0728bd266d72
WSW <2544894086@qq.com>
2020-09-08 22:32:25 +0800

    改个屁，不改了，fuck

--- wsw-client1/src/main/java/com/wsw/wswclient1/config/ClientWebSecurityConfigurer.java
+++ wsw-client1/src/main/java/com/wsw/wswclient1/config/ClientWebSecurityConfigurer.java
@@ -1,32 +1,11 @@
 package com.wsw.wswclient1.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wsw.module.entity.ResponseBean;
-import com.wsw.module.entity.User;
-import com.wsw.wswserver.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.*;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * @Author WangSongWen
@@ -41,66 +20,5 @@ public class ClientWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**").authorizeRequests().anyRequest().authenticated();
-              /*  .and()
-                .formLogin()
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .loginProcessingUrl("/doLogin")
-                .loginPage("/login")
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.setContentType("application/json;charset=utf-8");
-                        PrintWriter out = response.getWriter();
-                        User user = (User) authentication.getPrincipal();
-                        user.setPassword(null); //将返回给前端json中的密码隐藏
-                        ResponseBean ok = ResponseBean.ok("登录成功！", user);
-                        String s = new ObjectMapper().writeValueAsString(ok);
-                        out.write(s);
-                        out.flush();
-                        out.close();
-                    }
-                })
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
-                        response.setContentType("application/json;charset=utf-8");
-                        PrintWriter out = response.getWriter();
-                        ResponseBean error = ResponseBean.error("登录失败！");
-                        if (e instanceof LockedException){
-                            error.setMsg("账户被锁定，请联系管理员！");
-                        }else if (e instanceof CredentialsExpiredException){
-                            error.setMsg("密码过期，请联系管理员！");
-                        }else if (e instanceof AccountExpiredException){
-                            error.setMsg("账户过期，请联系管理员！");
-                        }else if (e instanceof DisabledException){
-                            error.setMsg("账户被禁用，请联系管理员！");
-                        }else if (e instanceof BadCredentialsException){
-                            error.setMsg("用户名或密码输入错误，请重新输入！");
-                        }
-                        String s = new ObjectMapper().writeValueAsString(error);
-                        out.write(s);
-                        out.flush();
-                        out.close();
-                    }
-                })
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.setContentType("application/json;charset=utf-8");
-                        PrintWriter out = response.getWriter();
-                        ResponseBean ok = ResponseBean.ok("注销成功！");
-                        String s = new ObjectMapper().writeValueAsString(ok);
-                        out.write(s);
-                        out.flush();
-                        out.close();
-                    }
-                })
-                .permitAll()
-                .and()
-                .csrf().disable();*/
     }
 }
