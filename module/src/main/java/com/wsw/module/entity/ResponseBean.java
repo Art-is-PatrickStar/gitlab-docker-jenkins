77d5f7c47a78c5bbb56e094719b628b0f105c977
wangsongwen <2544894086wsw@gmail.com>
2020-09-08 17:43:14 +0800

    登录信息响应类

---
+++ module/src/main/java/com/wsw/module/entity/ResponseBean.java
@@ -0,0 +1,61 @@
+package com.wsw.module.entity;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 17:00 2020/9/8
+ * @Description:
+ */
+public class ResponseBean {
+    private Integer status;
+    private String msg;
+    private Object object;
+
+    private ResponseBean() {
+    }
+
+    private ResponseBean(Integer status, String msg, Object object) {
+        this.status = status;
+        this.msg = msg;
+        this.object = object;
+    }
+
+    public static ResponseBean ok(String msg) {
+        return new ResponseBean(200, msg, null);
+    }
+
+    public static ResponseBean ok(String msg, Object object) {
+        return new ResponseBean(200, msg, object);
+    }
+
+    public static ResponseBean error(String msg) {
+        return new ResponseBean(500, msg, null);
+    }
+
+    public static ResponseBean error(String msg, Object object) {
+        return new ResponseBean(500, msg, object);
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
