2d1274169413ce8bb0928448320bdc47d6bdf5fc
wangsongwen <2544894086wsw@gmail.com>
2020-09-14 11:29:56 +0800

    去除JPA配置

--- wsw-client1/src/main/java/com/wsw/wswclient1/controller/LoginController.java
+++ wsw-client1/src/main/java/com/wsw/wswclient1/controller/LoginController.java
@@ -1,12 +1,12 @@
-package com.wsw.wswclient1.controller;
+package com.wsw.wswserver.controller;
 
-import com.wsw.wswclient1.entity.ResponseBean;
+import com.wsw.wswserver.entity.ResponseBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author WangSongWen
- * @Date: Created in 13:25 2020/9/10
+ * @Date: Created in 10:59 2020/9/14
  * @Description:
  */
 @RestController
---
+++ wsw-server/src/main/java/com/wsw/wswserver/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.wsw.wswserver.controller;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 10:59 2020/9/14
+ * @Description:
+ */
+public class LoginController {
+}
